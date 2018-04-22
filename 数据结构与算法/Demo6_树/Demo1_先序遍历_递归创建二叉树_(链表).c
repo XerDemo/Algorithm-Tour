07266ca5f75e6a728aebdf02bdece58128c1b4e8
XerDemo <1492113422@qq.com>
2018-04-22 21:48:11 +0800

    未完善

--- 数据结构与算法/Demo6_树/Demo1_先序遍历_递归创建二叉树_(链表).c
+++ 数据结构与算法/Demo6_树/Demo1_先序遍历_递归创建二叉树_(链表).c
@@ -3,7 +3,7 @@
 #define MAXSIZE 50
 
 /**
- *  用递归来创建二叉树树 递归遍历二叉树树
+ *  用递先序遍历的顺序 递归来创建二叉树树 递归遍历二叉树树
  */
 
 typedef int elemType;                        // 给int类型取个别名 方便更改数据域的类型
@@ -25,16 +25,20 @@ void preOrderTraverse(BiTNode * pT);         // 先序遍历
 void inOrderTraverse(BiTNode * pT);          // 中序遍历
 void postOrderTraverse(BiTNode * pT);        // 后序遍历
 void levelOrderTraverse(BiTNode * pT);       // 层次遍历  借助于 队列来实现
+int leafNum(BiTNode * pT);                   // 求叶节点个数   递归
+int nodeNum(BiTNode * pT);                   // 统计节点总数
+int treeDepth(BiTNode * pT);                 // 求二叉树的深度
+BiTNode * searchTree(BiTNode * pT, elemType val);   // 查找数据元素val 返回其地址
+
 
 Queue * createQueue();                       // 创建队列
-BiTNode *out_Queue(Queue *pQ);
-int is_empty(Queue * pQ);
-void in_Queue(Queue * pQ, BiTNode * pT);
-int is_full(Queue * pQ);
+BiTNode *out_Queue(Queue *pQ);               // 出队
+int is_empty(Queue * pQ);                    // 队列是否为空
+void in_Queue(Queue * pQ, BiTNode * pT);     // 入队
+int is_full(Queue * pQ);                     // 队列是否满
 
 int main(void)
 {
-
     BiTNode * pT = createTree();
     printf("先序遍历: ");
     preOrderTraverse(pT);
@@ -47,6 +51,15 @@ int main(void)
 
     printf("\n层次遍历: ");
     levelOrderTraverse(pT);
+    printf("\n");
+
+    printf("叶节点个数:%d\n", leafNum(pT));
+
+    printf("共有节点数：%d\n", nodeNum(pT));
+
+    printf("深度:%d\n", treeDepth(pT));
+
+    printf("数据元素为13的地址：%p\n", searchTree(pT, 13));
 
 }
 
@@ -106,13 +119,12 @@ void inOrderTraverse(BiTNode * pT)
 
 void postOrderTraverse(BiTNode * pT)
 {
-
     // 如果pT ！= NULL
     if (pT)
     {
         postOrderTraverse(pT->pLChild);       // 遍历左子树
         postOrderTraverse(pT->pRChild);       // 遍历右子树
-        printf(" %d ", pT->data);           // 遍历根节点
+        printf(" %d ", pT->data);             // 遍历根节点
     }
 }
 
@@ -138,10 +150,79 @@ void levelOrderTraverse(BiTNode * pT)
         {
             in_Queue(pQ, p->pRChild);           // 将根节点的右孩子入队
         }
-
     }
-
 }
+
+// 就是在递归先序遍历的基础上 加了一个判断条件而已
+int leafNum(BiTNode * pT)
+{
+    static int num = 0;
+    if (pT)
+    {
+        if (pT->pLChild == NULL && pT->pRChild == NULL)     // 如果 是叶子节点
+        {
+            num++;                              // 统计叶节点个数
+            printf("叶子节点：%d\n", pT->data);
+        }
+        leafNum(pT->pLChild);                   // 统计pT的左子树叶节点个数
+        leafNum(pT->pRChild);                   // 统计pT的右子树叶节点个数
+    }
+    return num;
+}
+
+// 节点总数
+int nodeNum(BiTNode * pT)
+{
+    static int num = 0;                         // 记录节点总数
+    if (pT)
+    {
+        num++;
+        nodeNum(pT->pLChild);
+        nodeNum(pT->pRChild);
+    }
+    return num;
+}
+
+// 求二叉树的深度
+int treeDepth(BiTNode * pT)
+{
+    int leftDepth, rightDepth;                  // leftDept：pT的左子数深度   rightDepth：pT的右子数深度
+    if (NULL == pT)                             // 如果数空则返回 0
+    {
+        return 0;
+    }
+    leftDepth = treeDepth(pT->pLChild);
+    rightDepth = treeDepth(pT->pRChild);
+    if (leftDepth > rightDepth){
+        return leftDepth + 1;                   // 返回左子树的深度  +  根节点：1
+    } else{
+        return rightDepth + 1;                  // 返回右子树深度 + 根节点
+    }
+}
+
+// 在二叉树中 查找某个元素 返回其地址
+
+BiTNode * searchTree(BiTNode * pT, int val)
+{
+    BiTNode * p = pT;
+    if (p!=NULL)                                     // 如果树不为空
+    {
+        if (val= p->data)                       // 如果根节点 就是要查找的节点 直接返回根  否则，分别在左右子树查找
+        {
+            return p;
+        }
+        p = searchTree(p->pLChild,val);         // 在以p->pLChild的根节点的二叉树中递归查找数据元素val
+        if (NULL != p)
+        {
+            return p;                           // 如果找到  返回p
+        } else{
+           return  p = searchTree(p->pRChild, val);
+        }
+    } else{
+        return NULL;                            // 查找失败 返回空
+    }
+}
+
 /******************创建队列根层次遍历有关********************/
 Queue * createQueue()
 {
--- 数据结构与算法/Demo6_树/Deom2_递归和非递归二叉树的实现_残次品(链表).c
+++ 数据结构与算法/Demo6_树/Deom2_递归和非递归二叉树的实现_残次品(链表).c
@@ -18,6 +18,12 @@ typedef struct stack{
     int top;                                                // 栈顶指针
 }Stack;
 
+// 定义队列
+typedef struct {
+    BTNode * Data[MAX_NODE];                 // 里面储存的是树节点的地址  -- 循环顺序队列
+    int front;                               // 队首指针 指向的是第一个有效元素的前面一个位置
+    int rear;                                // 队尾指针 指向的是最后一个有效元素的位置
+}Queue;
 
 
 
@@ -28,14 +34,22 @@ int emptyStack(Stack *pS);                                  // 判断堆栈是否空
 void push(Stack *pS, BTNode * pT);                          // 入栈
 BTNode * pop(Stack *pS);                                    // 出栈
 
+Queue * createQueue();                                      // 创建队列
+BTNode *out_Queue(Queue *pQ);
+int is_empty(Queue * pQ);
+void in_Queue(Queue * pQ, BTNode * pT);
+int is_full(Queue * pQ);
+
 void preOrderTraverse_1(BTNode * pT);                       // 递归先序遍历    不用堆栈
 void inOrderTraverse_1(BTNode * pT);                        // 递归中序遍历    不用堆栈
 void postOrderTraverse_1(BTNode * pT);                      // 递归后序遍历    不用堆栈
+int leafNum_1(BTNode * pT);                                 // 递归 求叶节点个数
 
 void preOrderTraverse_2(BTNode * pT);                       // 非递归 先序遍历 使用堆栈
 void inOrderTraverse_2(BTNode * pT);                        // 非递归 中序遍历 使用堆栈
-//void postOrderTraverse_2(BTNode * pT);                      // 非递归 后序遍历 使用堆栈
-
+//void postOrderTraverse_2(BTNode * pT);                    // 非递归 后序遍历 使用堆栈     未实现
+void levelOrderTraverse(BTNode * pT);                       // 非递归层次遍历
+int leafNum_2(BTNode * pT);                                 // 非递归 求叶子节点个数   对先序遍历进行修改
 
 
 int main(void)
@@ -50,15 +64,27 @@ int main(void)
     printf("\n递归后序遍历:");
     postOrderTraverse_1(pT);
 
+    printf("\n递归层次遍历:(暂无)");
+
+    printf("\n递归叶节点的个数：%d\n", leafNum_1(pT));
+
     printf("\n非递归先序遍历:");
     preOrderTraverse_2(pT);
 
     printf("\n非递归中序遍历:");
     inOrderTraverse_2(pT);
 
+    printf("\n非递归后序遍历:（暂无）");
+
+    printf("\n非递归层次遍历:");
+    inOrderTraverse_2(pT);
+
+    printf("\n非递归叶节点的个数：%d\n", leafNum_2(pT));
+
     return 0;
 }
 
+// 按层次遍历来创建树  输入编号和节点的值
 BTNode * createTree()
 {
     int idx;                                                // 输入节点的编号  从1开始 (0结束)
@@ -100,51 +126,9 @@ BTNode * createTree()
                 }
             }
         }
-
     }
     return pT;
 }
-
-Stack * createStack()
-{
-    Stack *pS = (Stack *) malloc(sizeof(Stack));
-    // 省略判断是否为 空
-    pS->top = -1;
-}
-
-int fullStack(Stack * pS)
-{
-    return MAX_NODE - 1 == pS->top;
-}
-
-int emptyStack(Stack * pS)
-{
-    return -1 == pS->top;
-}
-
-void push(Stack * pS, BTNode * pT)
-{
-    if (fullStack(pS))
-    {
-        printf("Stack is full! push is failed\n");
-        return;
-    }
-    pS->data[++pS->top] = pT;
-}
-
-BTNode * pop(Stack *pS)
-{
-    if (emptyStack(pS))
-    {
-        printf("Stack is empty! pop is failed!\n");
-    }
-    else
-    {
-        return pS->data[pS->top--];
-    }
-}
-
-
 
 
 // 递归先序遍历
@@ -224,5 +208,162 @@ void inOrderTraverse_2(BTNode * pT)
         }
     }
 }
+
+// 非递归层序遍历
+void levelOrderTraverse(BTNode * pT)
+{
+    // 如果是空树  直接返回
+    if (!pT)
+    {
+        return;
+    }
+    Queue *pQ = createQueue();                  // 创建队列
+    in_Queue(pQ, pT);                           // 树的根节点入队
+    // 只要队列不为空
+    while (!is_empty(pQ))
+    {
+        BTNode * p = out_Queue(pQ);            // 根节点出队
+        printf(" %d ", p->data);                // 打印根节点的值
+        if (p->pLChild)
+        {
+            in_Queue(pQ, p->pLChild);           // 将根节点的左孩子入队
+        }
+        if (p->pRChild)
+        {
+            in_Queue(pQ, p->pRChild);           // 将根节点的右孩子入队
+        }
+    }
+}
+
+// 递归 求叶子个数   按照递归先序遍历改
+int leafNum_1(BTNode * pT)
+{
+    static int num = 0;
+    // 如果树不空
+    if (pT)
+    {
+        if (pT->pLChild == NULL && pT->pRChild == NULL)
+        {
+            num++;
+        }
+        leafNum_1(pT->pLChild);
+        leafNum_1(pT->pRChild);
+    }
+    return num;
+}
+
+//// 求叶子节点个数（非递归）      写法错误
+//int leafNum_2(BTNode * pT)
+//{
+//    BTNode * p = pT;
+//    int num = 0;
+//    Stack *pS = createStack();
+//    while (p || !emptyStack(pS))
+//    {
+//        while (p)
+//        {
+//            push(pS, p);
+//            p = p->pLChild;
+//        }
+//        if (!emptyStack(pS))
+//        {
+//            p = pop(pS);
+//            p = p->pRChild;
+//            if (p == NULL)      // 跟非递归前序不同的是这里加了个判断条件   这一个如果执行相当于 左孩子右孩子都为NULL
+//            {
+//                num++;
+//            }
+//        }
+//    }
+//}
+
+/******************创建栈**************************/
+Stack * createStack()
+{
+    Stack *pS = (Stack *) malloc(sizeof(Stack));
+    // 省略判断是否为 空
+    pS->top = -1;
+}
+
+int fullStack(Stack * pS)
+{
+    return MAX_NODE - 1 == pS->top;
+}
+
+int emptyStack(Stack * pS)
+{
+    return -1 == pS->top;
+}
+
+void push(Stack * pS, BTNode * pT)
+{
+    if (fullStack(pS))
+    {
+        printf("Stack is full! push is failed\n");
+        return;
+    }
+    pS->data[++pS->top] = pT;
+}
+
+BTNode * pop(Stack *pS)
+{
+    if (emptyStack(pS))
+    {
+        printf("Stack is empty! pop is failed!\n");
+    }
+    else
+    {
+        return pS->data[pS->top--];
+    }
+}
+
+/******************创建队列根层次遍历有关********************/
+Queue * createQueue()
+{
+    Queue * pQ = (Queue *) malloc (sizeof(Queue));
+    if (!pQ)
+    {
+        printf("malloc is failed!\n");
+        exit(-1);
+    }
+    pQ->front = pQ->rear = 0;
+}
+
+int is_full(Queue * pQ)
+{
+    return pQ->front == (pQ->rear + 1) % MAX_NODE;
+}
+
+// 进队 只跟rear有关
+void in_Queue(Queue * pQ, BTNode * pT)
+{
+    if (is_full(pQ))
+    {
+        printf("Queue is full!\n");
+        return;
+    }
+    pQ->rear = (pQ->rear + 1) % MAX_NODE;
+    pQ->Data[pQ->rear] = pT;
+}
+
+int is_empty(Queue * pQ)
+{
+    return pQ->front == pQ->rear;
+}
+
+// 出对只跟 front有关
+BTNode *out_Queue(Queue *pQ)
+{
+    if (is_empty(pQ))
+    {
+        printf("Queue is empty!\n");
+    }
+    else
+    {
+        pQ->front = (pQ->front +1) % MAX_NODE;
+        return pQ->Data[pQ->front];
+    }
+}
+
 
 
